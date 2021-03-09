92a537ea48f6b6e3329a12fd08953d4c1106638a
GianRathgeb <gian.rathgeb@bbz-sh.ch>
2021-03-09 15:54:30 +0100

    encryption and decryption is now fully functioning

--- manager.py
+++ manager.py
@@ -1,36 +1,80 @@
 
 
-pw = "Password"
-key = "encrypt"
-
-binaryPW = [bin(ord(char)) for char in pw]
-test = ''
-for char in binaryPW:
-    test += char.replace('0b', '')
-
-binaryPW =  test
-
-binaryKey = [bin(ord(char)) for char in key]
-test = ''
-for char in binaryKey:
-    test += char.replace('0b', '')
-
-# repeat key until 
-binaryKey = (test * (int(len(binaryPW)/len(test))+1))[:len(binaryPW)]
+pw = "DE"
+key = "C"
 
 
+def encrypt(pw, key):
+    binaryPW = [bin(ord(char)) for char in pw]
+    test = ''
+    for char in binaryPW:
+        test += char.replace('0b', '')
 
-test= int(binaryPW,2) ^ int(binaryKey,2)
-xor = bin(test)[2:]
+    binaryPW =  test
+    print('binary PW ' + binaryPW)
 
-print(xor)
+    binaryKey = [bin(ord(char)) for char in key]
+    test = ''
+    for char in binaryKey:
+        test += char.replace('0b', '')
 
-result = []
-for index in range(0, len(xor), 8):
-    result.append(xor[index : index + 8])
+    # repeat key until 
+    binaryKey = (test * (int(len(binaryPW)/len(test))+1))[:len(binaryPW)]
 
-test = ''
-for i in result:
-    test  += str(int(i, 2))
+    print("bin key 1 " + binaryKey)
 
-print(test)
+    test= int(binaryPW,2) ^ int(binaryKey,2)
+    xor = bin(test)[2:]
+    xor = xor.zfill(len(binaryPW))
+
+    print("xor " + xor)
+
+    result = []
+    for index in range(0, len(xor), 7):
+        result.append(xor[index : index + 7])
+
+    test = ''
+    for i in result:
+        test  += str(int(i, 2)).zfill(3)
+
+    print('test encrypt ' + test)
+    return(test)
+
+
+def decrypt(string, key):
+    print('string ' + string)
+    test = []
+    for index in range(0, len(string), 3):
+        test.append(bin(int(string[index : index + 3])).replace("0b", "")) 
+    test1 = ""
+    for i in test:
+        test1 += i.zfill(7)
+    print("test1 " + test1)
+
+    binaryKey = [bin(ord(char)) for char in key]
+    test = ''
+    for char in binaryKey:
+        test += char.replace('0b', '')
+
+    # repeat key until 
+    binaryKey = (test * (int(len(test1)/len(test1))+1))[:len(test1)]
+    print("bin key 2 " + binaryKey)
+
+    test= int(test1,2) ^ int(binaryKey,2)
+    xor = bin(test)[2:]
+
+    print("xor 2 " + xor)
+
+    result = []
+    for index in range(0, len(xor), 7):
+        result.append(xor[index : index + 7])
+
+    test = ''
+    for i in result:
+        test  += chr(int(i, 2))
+
+    print(test)
+
+
+#encrypt(pw, key)
+decrypt(encrypt(pw, key), key)
