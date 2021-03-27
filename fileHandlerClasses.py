8f2c2e8853bbc4d046677f282199ce03af961ce6
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2021-03-27 11:31:30 +0100

    fileHandlerClass

---
+++ fileHandlerClasses.py
@@ -0,0 +1,27 @@
+import csv
+class FileWriter:
+    def __init__(self, fileName):
+        self.strFileName = fileName
+
+    def fnReadPasswords(self):
+        with open(self.strFileName) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            arrPasswords = []
+            for row in csv_reader:
+                try:
+                    arrPasswords.append(row[0])
+                except IndexError:
+                    print("No Passwords found in file")
+            return arrPasswords
+        
+    def fnWritePassword(self, strPassword):
+        with open(self.strFileName, mode='a+') as objPasswordFile:
+            objPasswordFile.write(f"\n{strPassword}")
+
+    def fnRewriteFile(self, strPasswordHash, arrPasswords):
+        with open(self.strFileName, mode='w') as objPasswordFile:
+            strToWrite = f"{strPasswordHash}"
+            for i in arrPasswords:
+                strToWrite += f"\n{i[0]}"
+            objPasswordFile.write(strToWrite)
+
