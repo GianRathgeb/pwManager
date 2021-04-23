cf7321d29f805128b6c6b3a3d171da0598427289
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2021-04-23 15:59:55 +0200

    Fixed encryption and decryption function

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import time
 import hashlib
+from dictNumbers import numberDict
 
 def isInt(s):
     try: 
@@ -8,48 +9,48 @@ def isInt(s):
     except ValueError:
         return False
 
+def xor(a, b):
+    ans = ""
+    # Loop to iterate over the
+    # Binary Strings
+    for i in range(len(a)):
+        # If the Character matches
+        if (str(a[i]) == str(b[i])): 
+            ans += "0"
+        else: 
+            ans += "1"
+    return ans     
+
+def fnStrToBin (StringToBin):
+    arrCharsBin = []
+    for char in [char for char in StringToBin]:
+        if isInt(char):
+            add = numberDict[int(char)].zfill(8)
+            arrCharsBin.append(f"{add}")
+        else:
+            add  = bin(ord(char)).replace('0b', '').zfill(8)
+            arrCharsBin.append(f"{add}")
+    returnStr = ''
+    for charBin in arrCharsBin:
+        returnStr += charBin
+    return returnStr
+
 def fnEncryptString(strPassword, strKey):
     # Password String to binary string
-    strBinaryPW = ''
-    arrCharsBin = []
-    for char in [char for char in strPassword]:
-        if isInt(char):
-            arrCharsBin.append(f"0{bin(int(char))}".replace('0b', ''))
-        else:
-            arrCharsBin.append(f"0{bin(ord(char))}".replace('0b', ''))
-    for charBin in arrCharsBin:
-        strBinaryPW += charBin
-
+    strBinaryPW = fnStrToBin(strPassword)
 
     # Key string to binary string
-    strBinaryKey = ''
-    arrCharsBinKey = []
-    for char in [char for char in strKey]:
-        if isInt(char):
-            arrCharsBinKey.append(f"{bin(int(char))}".replace('0b', ''))
-            print("int")
-        else:
-            arrCharsBinKey.append(f"{bin(ord(char))}".replace('0b', ''))
-    for charBin in arrCharsBinKey:
-        strBinaryKey += charBin
+    strBinaryKey = fnStrToBin(strKey)
     # repeat key until every bit from pw can be xor'ed with the key
     strBinaryKey = (strBinaryKey * (int(len(strBinaryPW)/len(strBinaryKey))+1))[:len(strBinaryPW)]
 
-
     # Perform the binary xor operation
-    binBinaryXor = int(strBinaryPW,2) ^ int(strBinaryKey,2)
-    # Cut the '0b' of the beginning and add missing 0 at the beginning
-    strXor = '0' + bin(binBinaryXor)[2:]
-
-    # fill the beginning with zeros so there are 7 binary numbers in the first char
-    strXor = strXor.zfill(len(strBinaryPW))
-
+    strXor = xor(strBinaryPW, strBinaryKey)
 
     # Create the cipher text with the result of the binary xor
     strCipherText = ''
-    for strcharcipherText in range(0, len(strXor), 7):
-        strCipherText += str(int(strXor[strcharcipherText : strcharcipherText + 7], 2)).zfill(3)
-        print(strXor[strcharcipherText : strcharcipherText + 7].zfill(3))
+    for strcharcipherText in range(0, len(strXor), 8):
+        strCipherText += str(int(strXor[strcharcipherText : strcharcipherText + 8], 2)).zfill(3)
     # return the cipher text
     return strCipherText
 
@@ -58,26 +59,22 @@ def fnDecryptString(strCiperText, strKey):
     # Ciphertext string to binary string
     strBinaryCipherText = ''
     for i in range(0, len(strCiperText), 3):
-        strBinaryCipherText += f"{bin(int(strCiperText[i : i + 3]))}".replace("0b", "").zfill(7)
+        strBinaryCipherText += f"{bin(int(strCiperText[i : i + 3]))}".replace("0b", "").zfill(8)
 
     # Key string to binary string
-    strBinaryKey = ''
-    for strBinaryCharKey in [bin(ord(char)) for char in strKey]:
-        strBinaryKey += strBinaryCharKey.replace('0b', '')
+    strBinaryKey = fnStrToBin(strKey)
     # repeat key until there is a bit for every bit in the cipher text
     strBinaryKey = (strBinaryKey * (int(len(strBinaryCipherText)/len(strBinaryKey))+1))[:len(strBinaryCipherText)]
 
 
     # perform the xor operation
-    strBinaryXor = int(strBinaryCipherText,2) ^ int(strBinaryKey,2)
-    # Cut of the '0b' at the beginning
-    binXorResult = bin(strBinaryXor)[2:]
+    binXorResult = xor(strBinaryCipherText, strBinaryKey)
 
 
     # create the clear text with the result of the binary xor
     strClearText = ''
-    for index in range(0, len(binXorResult), 7):
-        strClearText += chr(int(binXorResult[index : index + 7], 2))
+    for index in range(0, len(binXorResult), 8):
+        strClearText += chr(int(binXorResult[index : index + 8], 2))
 
     # return the clear text
     return strClearText
