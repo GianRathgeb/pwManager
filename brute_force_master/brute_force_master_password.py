a60a6da623049d370373a7594fff64c350a44b9e
gianrathgeb <gian.rathgeb@bluewin.ch>
2023-01-13 06:51:45 -0800

    split brute forcer to multiple files

--- brute_force_master/brute_force_master_password.py
+++ brute_force_master/brute_force_master_password.py
@@ -1,6 +1,7 @@
 import hashlib
 import progressbar
 import time
+from functions import fnEncryptString 
 
 # this code can be used to brute force the master password of the password database
 # do not use this for malicious purposes
@@ -17,100 +18,6 @@ password_list = '/usr/share/wordlists/rockyou.txt'
 #password_list = '/usr/share/wordlists/john.lst'
 
 
-
-#-------------------------------------------------------------------------------
-# Start functions section
-#-------------------------------------------------------------------------------
-
-
-# this is just a const of the number dict file:
-numberDict = {
-    0: "0110000",
-    1: "0110001",
-    2: "0110010",
-    3: "0110011",
-    4: "0110100",
-    5: "0110101",
-    6: "0110110",
-    7: "0110111",
-    8: "0111000",
-    9: "0111001"
-}
-
-# this is just the xor function of the original program
-def xor(a, b):
-    # perform the bitwise x-or on the a and b binary string inputted
-    ans = ""
-    # Loop to iterate over the
-    # Binary Strings
-    for i in range(len(a)):
-    # If the Character matches
-        if (str(a[i]) == str(b[i])):
-            ans += "0"
-        else:
-            ans += "1"
-    return ans
-    
-# this is the isInt function of the original program
-def isInt(s):
-    # check if an inputted character is type = string
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-    
-# this is the string to binary function of the original program
-def fnStrToBin(StringToBin):
-    # translate a string into a binary string
-    arrCharsBin = []
-    for char in [char for char in StringToBin]:
-        if isInt(char):
-            add = numberDict[int(char)].zfill(8)
-            arrCharsBin.append(f"{add}")
-        else:
-            add = bin(ord(char)).replace('0b', '').zfill(8)
-            arrCharsBin.append(f"{add}")
-    returnStr = ''
-    for charBin in arrCharsBin:
-        returnStr += charBin
-    return returnStr
-
-# this is the encryption function of the original program
-def fnEncryptString(strPassword, strKey):
-    # Password String to binary string
-    strBinaryPW = fnStrToBin(strPassword)
-
-    # Key string to binary string
-    strBinaryKey = fnStrToBin(strKey)
-    # repeat key until every bit from pw can be xor'ed with the key
-    try:
-        strBinaryKey = (
-            strBinaryKey * (int(len(strBinaryPW)/len(strBinaryKey))+1))[:len(strBinaryPW)]
-    except ZeroDivisionError:
-        # happens if no key give
-        exit()
-    # Perform the binary xor operation
-    strXor = xor(strBinaryPW, strBinaryKey)
-
-    # Create the cipher text with the result of the binary xor
-    strCipherText = ''
-    for strcharcipherText in range(0, len(strXor), 8):
-        strCipherText += str(
-            int(strXor[strcharcipherText: strcharcipherText + 8], 2)).zfill(3)
-    # return the cipher text
-    return strCipherText
-
-#-------------------------------------------------------------------------------
-# End functions section
-#-------------------------------------------------------------------------------
-
-
-
-
-#-------------------------------------------------------------------------------
-# Here starts the real program, the rest is mostly copied from the original program
-#-------------------------------------------------------------------------------
 
 # get the number of lines in the file:
 with open(password_list, 'r', encoding='latin-1') as password_file:
---
+++ brute_force_master/functions.py
@@ -0,0 +1,81 @@
+# Functions used by the brute forcer
+
+
+# this is just a const of the number dict file:
+numberDict = {
+    0: "0110000",
+    1: "0110001",
+    2: "0110010",
+    3: "0110011",
+    4: "0110100",
+    5: "0110101",
+    6: "0110110",
+    7: "0110111",
+    8: "0111000",
+    9: "0111001"
+}
+
+# this is just the xor function of the original program
+def xor(a, b):
+    # perform the bitwise x-or on the a and b binary string inputted
+    ans = ""
+    # Loop to iterate over the
+    # Binary Strings
+    for i in range(len(a)):
+    # If the Character matches
+        if (str(a[i]) == str(b[i])):
+            ans += "0"
+        else:
+            ans += "1"
+    return ans
+    
+# this is the isInt function of the original program
+def isInt(s):
+    # check if an inputted character is type = string
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+    
+# this is the string to binary function of the original program
+def fnStrToBin(StringToBin):
+    # translate a string into a binary string
+    arrCharsBin = []
+    for char in [char for char in StringToBin]:
+        if isInt(char):
+            add = numberDict[int(char)].zfill(8)
+            arrCharsBin.append(f"{add}")
+        else:
+            add = bin(ord(char)).replace('0b', '').zfill(8)
+            arrCharsBin.append(f"{add}")
+    returnStr = ''
+    for charBin in arrCharsBin:
+        returnStr += charBin
+    return returnStr
+
+# this is the encryption function of the original program
+def fnEncryptString(strPassword, strKey):
+    # Password String to binary string
+    strBinaryPW = fnStrToBin(strPassword)
+
+    # Key string to binary string
+    strBinaryKey = fnStrToBin(strKey)
+    # repeat key until every bit from pw can be xor'ed with the key
+    try:
+        strBinaryKey = (
+            strBinaryKey * (int(len(strBinaryPW)/len(strBinaryKey))+1))[:len(strBinaryPW)]
+    except ZeroDivisionError:
+        # happens if no key give
+        exit()
+    # Perform the binary xor operation
+    strXor = xor(strBinaryPW, strBinaryKey)
+
+    # Create the cipher text with the result of the binary xor
+    strCipherText = ''
+    for strcharcipherText in range(0, len(strXor), 8):
+        strCipherText += str(
+            int(strXor[strcharcipherText: strcharcipherText + 8], 2)).zfill(3)
+    # return the cipher text
+    return strCipherText
+
